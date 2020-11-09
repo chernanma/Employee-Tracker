e9e6a0e49d47904f138d9c960daa1adc35a29c61
Cesar Martinez <chernanma@msn.com>
2020-11-08 21:56:04 -0500

    Creating schema.sql for employee_tracker_db

---
+++ db/schema.sql
@@ -0,0 +1,51 @@
+-- Drops the programming_db if it already exists --
+DROP DATABASE IF EXISTS employee_tracker_db;
+
+-- Created the DB "wizard_schools_db" (only works on local connections)
+CREATE DATABASE employee_tracker_db;
+
+-- Use the DB wizard_schools_db for all the rest of the script
+USE employee_tracker_db;
+
+-- Created the table "deparment"
+CREATE TABLE department (
+  id int AUTO_INCREMENT NOT NULL,
+  name varchar(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+-- Created the table "role"
+CREATE TABLE role (
+  id int AUTO_INCREMENT NOT NULL, 
+  title varchar(30) NOT NULL, 
+  salary decimal(10,4) NOT NULL,
+  department_id int NOT NULL, 
+  PRIMARY KEY(id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+
+-- Created the table "employee"
+CREATE TABLE employee (
+  id int AUTO_INCREMENT NOT NULL,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(30) NOT NULL,  
+  role_id int NOT NULL,
+  manager_id int,
+  PRIMARY KEY(id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
+
+
+
+
+
+
+
+-- Inserted a set of records into the table
+INSERT INTO schools (name)
+VALUES ("Hogwarts School of Witchcraft");
+
+
